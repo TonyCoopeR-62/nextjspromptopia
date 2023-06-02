0422eb32595bd2f94dc0c2eb735c3c4481842232
Yaroslav Denisov <jar1991@mail.ru>
2023-06-02 22:16:18 +0700

    structure & auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,59 @@
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from '@utils/database';
+import User from '@models/user';
+import { ClientSession } from 'mongodb';
+
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    })
+  ],
+  // async sess3ion2({ session }: { session: { user: { email: string, id: string }}}) {
+  //   const sessionUser = await User.findOne({ email: session.user.email })
+  //   session.user.id = sessionUser._id.toString();
+  //   return session;
+  // },
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session?.user?.email});
+
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: sessionUser._id.toString(),
+        }
+      };
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+  
+        const userExists = await User.findOne({
+          email: profile?.email,
+        })
+  
+        if (!userExists) {
+          await User.create({
+            email: profile?.email,
+            username: profile?.name?.replace(' ', '').toLowerCase(),
+            image: profile?.image,
+          })
+        }
+  
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+
+
+})
+
+export { handler as GET, handler as POST };
---
+++ components/Provider.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { SessionProvider, SessionProviderProps } from "next-auth/react";
+
+interface ProviderProps {
+  children: React.ReactNode;
+  session: SessionProviderProps['session'];
+}
+
+const Provider = ({ children, session }: ProviderProps) => {
+  return (
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
+  )
+}
+
+export default Provider
